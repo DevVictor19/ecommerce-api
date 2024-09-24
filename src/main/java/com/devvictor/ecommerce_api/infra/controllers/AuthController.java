dcd2ab98823cd2a6025ea4e97aa3b11de91bfba7
DevVictor19 <antoniovictor12@live.com>
2024-09-24 20:25:51 -0300

    feat: implement auth controller

---
+++ src/main/java/com/devvictor/ecommerce_api/infra/controllers/AuthController.java
@@ -0,0 +1,40 @@
+package com.devvictor.ecommerce_api.infra.controllers;
+
+import com.devvictor.ecommerce_api.application.dtos.LoginUserRequestDTO;
+import com.devvictor.ecommerce_api.application.dtos.LoginUserResponseDTO;
+import com.devvictor.ecommerce_api.application.dtos.SignupUserRequestDTO;
+import com.devvictor.ecommerce_api.application.use_cases.LoginUserUseCase;
+import com.devvictor.ecommerce_api.application.use_cases.SignupUserUseCase;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final SignupUserUseCase signupUserUseCase;
+    private final LoginUserUseCase loginUserUseCase;
+
+    public AuthController(SignupUserUseCase signupUserUseCase,
+                          LoginUserUseCase loginUserUseCase) {
+        this.signupUserUseCase = signupUserUseCase;
+        this.loginUserUseCase = loginUserUseCase;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signup(@Valid @RequestBody SignupUserRequestDTO dto) {
+        signupUserUseCase.execute(dto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginUserResponseDTO> login(@Valid @RequestBody LoginUserRequestDTO dto) {
+
+        return ResponseEntity.ok(loginUserUseCase.execute(dto));
+    }
+}
