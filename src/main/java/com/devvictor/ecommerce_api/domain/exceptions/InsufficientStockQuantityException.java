80194fdcd19a7b94cf774955bd1ad983da0ac706
DevVictor19 <antoniovictor12@live.com>
2024-09-28 21:02:58 -0300

    feat: add domain exceptions

--- src/main/java/com/devvictor/ecommerce_api/domain/exceptions/InsufficientStockQuantityException.java
+++
@@ -1,7 +0,0 @@
-package com.devvictor.ecommerce_api.domain.exceptions;
-
-public class InsufficientStockQuantityException extends RuntimeException {
-    public InsufficientStockQuantityException() {
-        super("The quantity to collect is not available in stock");
-    }
-}
