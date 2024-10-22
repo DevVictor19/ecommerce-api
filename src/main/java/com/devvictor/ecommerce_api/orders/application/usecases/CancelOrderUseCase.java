8390f22e83ecc8feab4b0f5d0ec9d6c0f0230746
DevVictor19 <antoniovictor12@live.com>
2024-10-22 15:53:48 -0300

    feat: validate if order is paid before cancel it

--- src/main/java/com/devvictor/ecommerce_api/orders/application/usecases/CancelOrderUseCase.java
+++ src/main/java/com/devvictor/ecommerce_api/orders/application/usecases/CancelOrderUseCase.java
@@ -1,12 +1,12 @@
 package com.devvictor.ecommerce_api.orders.application.usecases;
 
-import com.devvictor.ecommerce_api.shared.application.exceptions.NotFoundException;
 import com.devvictor.ecommerce_api.orders.application.services.OrderService;
 import com.devvictor.ecommerce_api.orders.domain.entities.Order;
+import com.devvictor.ecommerce_api.orders.domain.enums.OrderStatus;
+import com.devvictor.ecommerce_api.shared.application.exceptions.BadRequestException;
+import com.devvictor.ecommerce_api.shared.application.exceptions.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -14,12 +14,14 @@ public class CancelOrderUseCase {
     private final OrderService orderService;
 
     public void execute(String orderId, String userId) {
-        Optional<Order> order = orderService.findByIdAndUserId(orderId, userId);
+       Order order = orderService.findByIdAndUserId(orderId, userId)
+               .orElseThrow(() -> new NotFoundException("Order not found"));
 
-        if (order.isEmpty()) {
-            throw new NotFoundException("Order not found");
-        }
 
-        orderService.delete(order.get());
+       if (order.getStatus() != OrderStatus.WAITING_PAYMENT) {
+           throw new BadRequestException("This order cant be canceled");
+       }
+
+        orderService.delete(order);
     }
 }
