4b660f80b0619cc5bce71d149a207ee639b94caf
DevVictor19 <antoniovictor12@live.com>
2024-10-30 14:40:10 -0300

    feat: return no content when cart is empty

--- src/main/java/com/devvictor/ecommerce_api/cart/application/usecases/FindCartByUserUseCase.java
+++ src/main/java/com/devvictor/ecommerce_api/cart/application/usecases/FindCartByUserUseCase.java
@@ -1,6 +1,5 @@
 package com.devvictor.ecommerce_api.cart.application.usecases;
 
-import com.devvictor.ecommerce_api.shared.application.exceptions.NotFoundException;
 import com.devvictor.ecommerce_api.cart.application.services.CartService;
 import com.devvictor.ecommerce_api.cart.domain.entities.Cart;
 import lombok.RequiredArgsConstructor;
@@ -13,13 +12,7 @@ import java.util.Optional;
 public class FindCartByUserUseCase {
     private final CartService cartService;
 
-    public Cart execute(String userId) {
-        Optional<Cart> cart = cartService.findByUserId(userId);
-
-        if (cart.isEmpty()) {
-            throw new NotFoundException("Cart not found");
-        }
-
-        return cart.get();
+    public Optional<Cart> execute(String userId) {
+       return cartService.findByUserId(userId);
     }
 }
