bd13b1cb03c11bb17ab6ec8c7d40cf0192d74e39
DevVictor19 <antoniovictor12@live.com>
2024-11-02 14:11:15 -0300

    feat: subtract products from stock when creating an order

--- src/main/java/com/devvictor/ecommerce_api/orders/application/usecases/CreateOrderUseCase.java
+++ src/main/java/com/devvictor/ecommerce_api/orders/application/usecases/CreateOrderUseCase.java
@@ -1,43 +1,49 @@
 package com.devvictor.ecommerce_api.orders.application.usecases;
 
-import com.devvictor.ecommerce_api.shared.application.exceptions.NotFoundException;
 import com.devvictor.ecommerce_api.cart.application.services.CartService;
-import com.devvictor.ecommerce_api.orders.application.services.OrderService;
 import com.devvictor.ecommerce_api.cart.domain.entities.Cart;
+import com.devvictor.ecommerce_api.orders.application.services.OrderService;
 import com.devvictor.ecommerce_api.orders.domain.entities.Order;
 import com.devvictor.ecommerce_api.orders.domain.entities.OrderCart;
 import com.devvictor.ecommerce_api.orders.domain.factories.OrderCartFactory;
 import com.devvictor.ecommerce_api.orders.domain.factories.OrderFactory;
+import com.devvictor.ecommerce_api.products.application.services.ProductService;
+import com.devvictor.ecommerce_api.shared.application.exceptions.BadRequestException;
+import com.devvictor.ecommerce_api.shared.application.exceptions.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
+import java.util.concurrent.CompletionException;
 
 @Component
 @RequiredArgsConstructor
 public class CreateOrderUseCase {
     private final CartService cartService;
     private final OrderService orderService;
+    private final ProductService productService;
 
     public void execute(String userId) {
-        Optional<Cart> cart = cartService.findByUserId(userId);
+        Cart cart = cartService.findByUserId(userId)
+                .orElseThrow(() -> new NotFoundException("Cart not found"));
 
-        if (cart.isEmpty()) {
-            throw new NotFoundException("Cart not found");
+        try {
+            productService.subtractProductsFromStock(cart.getProducts()).join();
+        } catch (CompletionException e) {
+            Throwable throwable = e.getCause();
+            throw new BadRequestException("Products not available", throwable);
         }
 
         OrderCart orderCart = OrderCartFactory.create(
-                cart.get().getId(),
-                cart.get().getProducts(),
-                cart.get().getProductsQuantity(),
-                cart.get().getTotalPrice(),
-                cart.get().getCreatedAt()
+                cart.getId(),
+                cart.getProducts(),
+                cart.getProductsQuantity(),
+                cart.getTotalPrice(),
+                cart.getCreatedAt()
         );
 
         Order order = OrderFactory.create(userId, orderCart);
-
         orderService.create(order);
 
-        cartService.delete(cart.get());
+        cartService.delete(cart);
     }
 }
